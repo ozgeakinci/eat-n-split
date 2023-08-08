bd2f547914e55f449b723bba63ead8c491a7bd90
ozge <ozge.belenn@gmail.com>
2023-08-08 13:10:52 +0300

    yeni arkadaş ekleme

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.css";
 
 const initialFriends = [
@@ -23,22 +23,41 @@ const initialFriends = [
 ];
 
 const App = () => {
+  const [showAddFriend, setShowAddFriend] = useState(false);
+  // yeni yarattığımız newFriend için state
+  const [friends, setFriends] = useState(initialFriends);
+
+  // newFriend eklediğimiz fonksiyon
+  const handleAddFriend = (friend) => {
+    setFriends((friends) => [...friends, friend]);
+  };
+
+  //yeni arkadaş ekleme butonuna bastığımızda açılmasını sağlar.
+  const handleShowFriend = () => {
+    // FormAddFriend'in Buttona tıklandığunda açılabilmesi için state ayarlandı.
+    // setShowAddFriend(!showAddFriend); bu şekilde de olabilir ancak callback kullanmak çok daha iyi
+    setShowAddFriend((show) => !show);
+  };
+
   return (
     <div className="app">
       <div className="sidebar">
-        <FriendsList />
-        <FormAddFriend />
-        <Button> Add friend</Button>
+        <FriendsList friends={friends} />
+        {/* Burda add friend butonuna bastığımızda FormAddFriend componentini gösterir */}
+        {showAddFriend && <FormAddFriend handleAddFriend={handleAddFriend} />}
+        <Button onClick={handleShowFriend}>
+          {/* Butondaki textin değişmesi için state kullanıldı */}
+          {showAddFriend ? "Close" : "Add friend"}
+        </Button>
       </div>
+      <FormSplitBill />
     </div>
   );
 };
 
 export default App;
 
-const FriendsList = () => {
-  const friends = initialFriends;
-
+const FriendsList = ({ friends }) => {
   return (
     <ul>
       {friends.map((friend) => (
@@ -73,18 +92,80 @@ const Friend = ({ friend }) => {
   );
 };
 
-const Button = ({ children }) => {
-  return <button className="button">{children}</button>;
+const Button = ({ children, onClick }) => {
+  return (
+    <button className="button" onClick={onClick}>
+      {children}
+    </button>
+  );
 };
 
-const FormAddFriend = () => {
+const FormAddFriend = ({ handleAddFriend }) => {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("https://i.pravatar.cc/48");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // eğer if ve image yoksa döndür
+    if (!name || !image) return;
+
+    const id = crypto.randomUUID();
+    //yeni bir friend eklemek için
+    const newFriend = {
+      name,
+      image: `${image}?=$${id}`,
+      id,
+      balance: 0,
+    };
+
+    handleAddFriend(newFriend);
+
+    setName("");
+    setImage("https://i.pravatar.cc/48");
+  };
   return (
-    <form className="form-add-friend">
+    <form className="form-add-friend" onSubmit={handleSubmit}>
       <label htmlFor="">👭Friend Name</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+
       <label htmlFor=""> 👭 Image URL</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={image}
+        onChange={(e) => setImage(e.target.value)}
+      />
+
       <Button> Add</Button>
+    </form>
+  );
+};
+
+const FormSplitBill = () => {
+  return (
+    <form className="form-split-bill">
+      <h2 htmlFor="">SPLİT A BILL WİTH X</h2>
+
+      <label htmlFor="">💰 Bill value</label>
+      <input type="text" />
+
+      <label htmlFor=""> 🙎‍♂️Your expense</label>
+      <input type="text" />
+
+      <label htmlFor="">👭 X expense</label>
+      <input type="text" />
+
+      <label htmlFor="">Who is paying the bill</label>
+      <select name="" id="">
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
+
+      <Button>Split Bill</Button>
     </form>
   );
 };
